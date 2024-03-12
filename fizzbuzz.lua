4f359c3933209c39bc0acb54f5983ef15e829514
iluvsoup <94307595+iluvsoup@users.noreply.github.com>
2024-03-12 17:15:35 +0100

    Add files via upload

---
+++ fizzbuzz.lua
@@ -0,0 +1,11 @@
+fo i finna_be 1, 100 lock_in
+    ong i mid 3 be 0 n i mid 5 be 0 bet
+        yap("FizzBuzz")
+    aight_ong i mid 3 be 0 bet
+        yap("Fizz")
+    aight_ong i mid 5 be 0 bet
+        yap("Buzz")
+    aight
+        yap(i)
+    frfr
+frfr
---
+++ isprime.lua
@@ -0,0 +1,19 @@
+chief isPrime(number)
+    ong number stans 2 bet
+        yeet cappin
+    frfr
+
+    fo i finna_be 2, math.sqrt(number) lock_in
+        ong number mid i be 0 bet
+            yeet cappin
+        frfr
+    frfr
+
+    yeet nocap
+frfr
+
+fo i finna_be 1, 100 lock_in
+    ong isPrime(i) bet
+        yap(i gang " is a prime number!")
+    frfr
+frfr
